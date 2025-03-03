005d6829b06e1bb89ee74b580e86f58cd0bbeb4f
WebDevJ <WebDevJ@users.noreply.github.com>
2025-03-03 17:00:06 -0500

    text update: TODO: get rid of inline style

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import TaxBracketCalculator from "./components/TaxBracketCalculator";
 
 const App: React.FC = () => {
   const [taxableIncome, setTaxableIncome] = useState<number | null>(null);
-
+  //TODO: get rid of inline style during cleanUp code session"
   return (
     <div className="container">
       <h1>
-        <span className="name" style={{ color: "--bright-orange" }}>Tax</span>Man App.
+        <span style={{ color: "#d86541" }}>Tax</span>Man App.
       </h1>
       <TaxForm setTaxableIncome={setTaxableIncome} />
       {taxableIncome !== null && <TaxBracketCalculator taxableIncome={taxableIncome} />}
